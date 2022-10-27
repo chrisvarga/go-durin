9993ba8e42116dc677fd87f36893a1d8d12c3ce9
Chris Varga <chrisvarga@users.noreply.github.com>
2022-10-27 02:04:12 +0000

    Remove log

--- go-durin.go
+++ go-durin.go
@@ -3,21 +3,20 @@ package durin
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"net"
 )
 
 func durin(message string) string {
-    conn, err := net.Dial("tcp", "localhost:8045")
+	conn, err := net.Dial("tcp", "localhost:8045")
 	if err != nil {
 		return "(error) failed to connect to durin"
 	}
 	defer conn.Close()
-    fmt.Fprintf(conn, "%s\n", message)
+	fmt.Fprintf(conn, "%s\n", message)
 	reader := bufio.NewReader(conn)
-	response, err = reader.ReadString('\n')
+	data, err := reader.ReadString('\n')
 	if err != nil {
 		return "(error) connection lost"
 	}
-	return response
+	return data
 }
