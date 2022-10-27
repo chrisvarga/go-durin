874745cc0d08ab0a0c7272984a684b6cdcdb02c9
Chris Varga <chrisvarga@users.noreply.github.com>
2022-10-27 02:31:32 +0000

    Add error framework

--- go-durin.go
+++ go-durin.go
@@ -2,21 +2,22 @@ package durin
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"net"
 )
 
-func Durin(message string) string {
+func Durin(message string) (string, error) {
 	conn, err := net.Dial("tcp", "localhost:8045")
 	if err != nil {
-		return "(error) failed to connect to durin"
+		return "", errors.New("(error) failed to connect to durin")
 	}
 	defer conn.Close()
 	fmt.Fprintf(conn, "%s\n", message)
 	reader := bufio.NewReader(conn)
 	data, err := reader.ReadString('\n')
 	if err != nil {
-		return "(error) connection lost"
+		return "", errors.New("(error) connection lost")
 	}
-	return data
+	return data, nil
 }
---
+++ readme.md
@@ -0,0 +1,30 @@
+# Durin client for Go
+go-durin provides a simple interface for interacting with the durin database in Go.
+
+## Installation
+To install with go get, do:
+```shell
+go get github.com/chrisvarga/go-durin
+```
+
+## Quickstart
+```go
+import (
+    "github.com/chrisvarga/go-durin"
+    "fmt"
+)
+
+func ExampleClient() {
+    val, err := durin.Durin("set key value")
+    if err != nil {
+        panic(err)
+    }
+    val, err := durin.Durin("get key")
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println("key", val)
+    // Output: key value
+}
+```
+
